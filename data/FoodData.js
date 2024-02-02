e87ff2819e7fe7ab5d03640b51cc8310b7e216f2
Wajid-Ali-Cloud <abode.wajid@gmail.com>
2024-02-02 17:27:13 +0500

    fdgdf

---
+++ data/FoodData.js
@@ -0,0 +1,117 @@
+const data = [
+  {
+    key: '1',
+    name: 'Ice-Cream',
+    image: require('../assets/1.jpg'),
+    category: 'Fast Food',
+    category_id: '1',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    roti: [
+      {
+        id: '1',
+        name: 'Roti',
+      },
+      {
+        id: '2',
+        name: 'Khamiri Roti',
+      },
+    ],
+  },
+  {
+    key: '2',
+    name: 'Pizza',
+    image: require('../assets/2.jpg'),
+    category: 'Fast Food',
+    category_id: '2',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    roti: [
+      {
+        id: '1',
+        name: 'Roti',
+      },
+      {
+        id: '2',
+        name: 'Khamiri Roti',
+      },
+    ],
+  },
+  {
+    key: '3',
+    name: 'Sandwich',
+    image: require('../assets/3.jpg'),
+    category: 'Fast Food',
+    category_id: '3',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    roti: [
+      {
+        id: '1',
+        name: 'Roti',
+      },
+      {
+        id: '2',
+        name: 'Khamiri Roti',
+      },
+    ],
+  },
+  {
+    key: '4',
+    name: 'Salad',
+    image: require('../assets/1.jpg'),
+    category: 'Healthy',
+    category_id: '4',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    roti: [
+      {
+        id: '1',
+        name: 'Roti',
+      },
+      {
+        id: '2',
+        name: 'Khamiri Roti',
+      },
+    ],
+  },
+  {
+    key: '5',
+    name: 'Sushi',
+    image: require('../assets/2.jpg'),
+    category: 'Japanese',
+    category_id: '5',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    roti: [
+      {
+        id: '1',
+        name: 'Roti',
+        quantity: '3',
+      },
+      {
+        id: '2',
+        name: 'Khamiri Roti',
+        quantity: '3',
+      },
+    ],
+  },
+  {
+    key: '6',
+    name: 'Pasta',
+    image: require('../assets/3.jpg'),
+    category: 'Italian',
+    category_id: '6',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    roti: [
+      {
+        id: '1',
+        name: 'Roti',
+        quantity: '3',
+      },
+      {
+        id: '2',
+        name: 'Khamiri Roti',
+        quantity: '3',
+      },
+    ],
+  },
+  // Add more food items as needed
+];
+
+export default data;
